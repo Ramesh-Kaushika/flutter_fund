a49e94c9130a1b7733ef45af61ccee7b5b21dafe
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-15 01:27:34 +0530

    App 01

--- lib/main.dart
+++ lib/main.dart
@@ -470,6 +470,153 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "Layout 07",
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         backgroundColor: Colors.black,
+//         appBar: AppBar(
+//           backgroundColor: Colors.amber[900],
+//         ),
+//         body: SingleChildScrollView(
+//           child: Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 10),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Container(
+//                   padding: const EdgeInsets.all(20),
+//                   width: double.infinity,
+//                   height: 450,
+//                   decoration: BoxDecoration(
+//                     color: Colors.amber[300],
+//                     borderRadius: BorderRadius.circular(20),
+//                   ),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       const Text("User Interfaces with Flutter"),
+//                       const SizedBox(
+//                         height: 20,
+//                       ),
+//                       const Text(
+//                         '''In Flutter, the user interface is represented as a tree of widgets,commonly known as the widget tree.Each widget in the tree corresponds to a specific UI component, and the arrangement of these widgets defines the layout and appearance of the app.By understanding the widget tree, you can efficiently organize your UI components and create a seamless user experience.''',
+//                       ),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           Container(
+//                             width: 150,
+//                             height: 150,
+//                             decoration: BoxDecoration(
+//                                 color: const Color.fromARGB(255, 255, 0, 208),
+//                                 borderRadius: BorderRadius.circular(20)),
+//                           ),
+//                           const SizedBox(
+//                             height: 200,
+//                           ),
+//                           Container(
+//                             width: 150,
+//                             height: 150,
+//                             decoration: BoxDecoration(
+//                                 color: const Color.fromARGB(255, 255, 0, 208),
+//                                 borderRadius: BorderRadius.circular(20)),
+//                           ),
+//                         ],
+//                       )
+//                     ],
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                         height: 20,
+//                       ),
+//                 Container(
+//                   height: 450,
+//                   decoration: BoxDecoration(
+//                     color: Colors.amber[300],
+//                     borderRadius: BorderRadius.circular(20),
+//                   ),
+//                   child: Column(
+//                     children: [
+//                       const Row(
+//                         children: [
+//                           Text("open-source"),
+//                           Icon(Icons.abc_outlined)
+//                         ],
+//                       ),
+//                       const Text(
+//                           "Flutter is an open-source UI (User Interface) software development kit created by Google.Flutter is an open-source UI (User Interface) software development kit created by Google."),
+//                       const SizedBox(
+//                         height: 20,
+//                       ),
+//                       Container(
+//                         width: 350,
+//                         height: 250,
+//                         decoration: BoxDecoration(
+//                           color: const Color.fromARGB(255, 255, 0, 208),
+//                           borderRadius: BorderRadius.circular(20),
+//                         ),
+//                         child: Column(
+//                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                           children: [
+//                             const Text("open-source"),
+//                             const Text(
+//                                 "Flutter is an open-source UI (User Interface) software development kit created by Google.Flutter is an open-source UI (User Interface) software development kit created by Google."),
+//                             Row(
+//                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                               children: [
+//                                 Container(
+//                                   height: 60,
+//                                   width: 60,
+//                                   decoration: BoxDecoration(
+//                                     borderRadius: BorderRadius.circular(100),
+//                                     color: Colors.amber
+//                                   ),
+//                                 ),
+//                                 Container(
+//                                   height: 60,
+//                                   width: 60,
+//                                   decoration: BoxDecoration(
+//                                     borderRadius: BorderRadius.circular(100),
+//                                     color: Colors.amber
+//                                   ),
+//                                 ),
+//                                 Container(
+//                                   height: 60,
+//                                   width: 60,
+//                                   decoration: BoxDecoration(
+//                                     borderRadius: BorderRadius.circular(100),
+//                                     color: Colors.amber
+//                                   ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -482,134 +629,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Layout 07",
+      title: "App 01",
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          backgroundColor: Colors.amber[900],
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  width: double.infinity,
-                  height: 450,
-                  decoration: BoxDecoration(
-                    color: Colors.amber[300],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text("User Interfaces with Flutter"),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        '''In Flutter, the user interface is represented as a tree of widgets,commonly known as the widget tree.Each widget in the tree corresponds to a specific UI component, and the arrangement of these widgets defines the layout and appearance of the app.By understanding the widget tree, you can efficiently organize your UI components and create a seamless user experience.''',
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 150,
-                            height: 150,
-                            decoration: BoxDecoration(
-                                color: const Color.fromARGB(255, 255, 0, 208),
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          const SizedBox(
-                            height: 200,
-                          ),
-                          Container(
-                            width: 150,
-                            height: 150,
-                            decoration: BoxDecoration(
-                                color: const Color.fromARGB(255, 255, 0, 208),
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                        height: 20,
-                      ),
-                Container(
-                  height: 450,
-                  decoration: BoxDecoration(
-                    color: Colors.amber[300],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      const Row(
-                        children: [
-                          Text("open-source"),
-                          Icon(Icons.abc_outlined)
-                        ],
-                      ),
-                      const Text(
-                          "Flutter is an open-source UI (User Interface) software development kit created by Google.Flutter is an open-source UI (User Interface) software development kit created by Google."),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        width: 350,
-                        height: 250,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 255, 0, 208),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Text("open-source"),
-                            const Text(
-                                "Flutter is an open-source UI (User Interface) software development kit created by Google.Flutter is an open-source UI (User Interface) software development kit created by Google."),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Container(
-                                  height: 60,
-                                  width: 60,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    color: Colors.amber
-                                  ),
-                                ),
-                                Container(
-                                  height: 60,
-                                  width: 60,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    color: Colors.amber
-                                  ),
-                                ),
-                                Container(
-                                  height: 60,
-                                  width: 60,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    color: Colors.amber
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          title: const Text("App 01"),
+          backgroundColor: Colors.orange[900],
+          leading: const Icon(
+            Icons.menu,
+            size: 30,
           ),
         ),
       ),
